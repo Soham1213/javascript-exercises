062209841b188898e03200bf7e76a20fa4d7432c
Soham1213 <sohambih@gmail.com>
2022-08-26 14:51:31 -0700

    Added all conditions

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let temp = 0;
+    if (year % 100 != 0 && year % 400 == 0) {
+            return true;
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
