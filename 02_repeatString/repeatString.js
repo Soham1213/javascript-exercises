77f7e4a61208fd8a6d69e5f6ae7e7cf8c9645a3f
Soham1213 <sohambih@gmail.com>
2022-08-26 13:35:42 -0700

    Cleaned up code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,11 +3,13 @@ const repeatString = function(str, times) {
     if (times < 0) {
         return 'ERROR';
     }
-    for (let i = 0; i < times; i++)
-    {
-        repeat += str;
-    }
+    else {
+        for (let i = 0; i < times; i++)
+        {
+            repeat += str;
+        }
     return repeat;
+    }
 };
 // Do not edit below this line
 module.exports = repeatString;
