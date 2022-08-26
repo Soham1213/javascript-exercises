4a59410a5093c57bf0e16e986abb33cd52342d6b
Soham1213 <sohambih@gmail.com>
2022-08-26 14:32:56 -0700

    Added ERROR catches

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(start, end) {
     let sum = 0;
-    if (typeof start === 'number' || typeof end === 'number'){
+    if (typeof start === 'number' && typeof end === 'number' && start > 0 && end > 0){
         if (start > end) {
             for (let i = start; i >= end; i--) {
                 sum+=i;
