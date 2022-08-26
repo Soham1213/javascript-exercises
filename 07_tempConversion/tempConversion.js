3a52ca17c0d133c81839cd491306160558239462
Soham1213 <sohambih@gmail.com>
2022-08-26 15:06:31 -0700

    Added rounding

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  let cel = (fah - 32) * 5/9;
+  let celRound = Math.round(cel * 10) / 10;
+  return celRound;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let fah = (cel * 9/5) + 32;
+  let fahRound = Math.round(fah * 10) / 10;
+  return fahRound;
 };
 
 // Do not edit below this line
